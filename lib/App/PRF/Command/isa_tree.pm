3600bd6e69c8a2714b5c4b8662b8c72a89d28615
vti <viacheslav.t@gmail.com>
2012-06-19 12:55:03 +0300

    Added isa_list command

---
+++ lib/App/PRF/Command/isa_tree.pm
@@ -0,0 +1,123 @@
+package App::PRF::Command::isa_tree;
+
+use strict;
+use warnings;
+
+use base 'App::PRF::Command';
+
+use Class::Load ();
+use PPI;
+use File::Find ();
+
+sub BUILD {
+    my $self = shift;
+
+    $self->{color} = 1 unless defined $self->{color};
+
+    $self->{root} ||= './lib';
+
+    return $self;
+}
+
+sub run {
+    my $self = shift;
+    my ($root) = @_;
+
+    $root ||= $self->{root};
+    $root = File::Spec->rel2abs($root);
+
+    die "Can't open '$root': $!\n" unless -d $root;
+
+    my @files;
+    File::Find::find(
+        {   wanted => sub {
+                return unless /\.p(m|l)$/;
+
+                push @files, $_;
+            },
+            no_chdir => 1,
+            follow   => 0
+        },
+        $root
+    );
+
+    my %packages;
+
+    unshift @INC, $root;
+    foreach my $file (@files) {
+        my $ppi = PPI::Document->new($file, readonly => 1);
+
+        my $package = $ppi->find_first('PPI::Statement::Package');
+        next unless $package && $package->namespace;
+        $package = $package->namespace;
+
+        eval {
+            Class::Load::load_class($package);
+
+            $packages{$package} ||= {};
+        } || do {
+
+            warn $@;
+        };
+    }
+
+    foreach my $package (keys %packages) {
+        no strict;
+        my @isa = grep {exists $packages{$_}} @{"$package\::ISA"};
+
+        $packages{$package} = {isa => [@isa]};
+    }
+
+    my $isa_walker;
+    $isa_walker = sub {
+        my ($package) = @_;
+
+        foreach my $isa (@{$packages{$package}->{isa}}) {
+            push @{$packages{$isa}->{children}}, $package
+              unless grep { $_ eq $package } @{$packages{$isa}->{children}};
+
+            $isa_walker->($isa);
+        }
+    };
+
+    foreach my $package (sort keys %packages) {
+        $isa_walker->($package);
+    }
+
+    my @roots;
+    foreach my $package (sort keys %packages) {
+
+        my $is_a_child = 0;
+        foreach my $p (sort keys %packages) {
+            if (grep {$_ eq $package} @{$packages{$p}->{children}}) {
+                $is_a_child = 1;
+                last;
+            }
+        }
+
+        if (!$is_a_child) {
+            push @roots, $package;
+        }
+    }
+
+    my $child_walker; $child_walker = sub {
+        my ($root, $offset) = @_;
+
+        foreach my $child (@{$packages{$root}->{children}}) {
+            for (1 .. $offset / 2) {
+                print ' + ';
+            }
+            print $child, "\n";
+
+            $child_walker->($child, $offset + 2);
+        }
+    };
+
+    foreach my $root (@roots) {
+        print $root, "\n";
+
+        $child_walker->($root, 2);
+    }
+}
+
+1;
---
+++ t/tests/isa_treeTest.pm
@@ -0,0 +1,97 @@
+package isa_treeTest;
+
+use strict;
+use warnings;
+
+use base 'TestBase';
+
+use Test::More;
+use Test::Fatal;
+
+use File::Path ();
+use File::Basename ();
+use File::Spec;
+
+use App::PRF::Command::isa_tree;
+
+sub isa_tree : Test {
+    my $self = shift;
+
+    my $command = $self->_build_command(root => $self->_create_root);
+
+    $self->_mkdir('Foo/Bar');
+    $self->_mkfile('Foo/Bar/Baz.pm', q/package Foo::Bar::Baz; sub new {} 1;/);
+
+    $self->_mkfile('Main.pm', q/package Main; use base 'Foo::Bar::Baz'; sub new {} 1;/);
+
+    my $output = '';
+
+    open my $stdout, ">&", STDOUT;
+    close STDOUT;
+    open STDOUT, '>', \$output or die "Failed to capture stdout: $!";
+
+    $command->run($self->{root});
+
+    close STDOUT;
+    open STDOUT, ">&", $stdout;
+    close $stdout;
+
+    is($output, "Foo::Bar::Baz\n + Main\n");
+}
+
+sub _build_command {
+    my $self = shift;
+
+    return App::PRF::Command::isa_tree->new(@_);
+}
+
+sub _slurp {
+    my $self = shift;
+    my ($file) = @_;
+
+    local $/;
+    open my $fh, '<', File::Spec->catfile($self->{root}, $file);
+    return <$fh>;
+}
+
+sub _mkdir {
+    my $self = shift;
+    my ($dir) = @_;
+
+    File::Path::make_path(File::Spec->catfile($self->{root}, $dir));
+
+    return;
+}
+
+sub _mkfile {
+    my $self = shift;
+    my ($file, $content) = @_;
+
+    open my $fh, '>', File::Spec->catfile($self->{root}, $file);
+    print $fh $content;
+
+    return;
+}
+
+sub _create_root {
+    my $self = shift;
+
+    my $root =
+      File::Spec->catfile(File::Basename::dirname(__FILE__), '../isa_tree');
+    $self->{root} = $root;
+
+    if (-e $root) {
+        if (-d $root) {
+            File::Path::remove_tree($root);
+        }
+        else {
+            die 'Smth is wrong';
+        }
+    }
+
+    mkdir $root;
+
+    return $self->{root};
+}
+
+1;
