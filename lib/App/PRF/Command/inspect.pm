021a6acf387d5d7b03ae8e0a17aa8220ad88414d
vti <viacheslav.t@gmail.com>
2012-06-19 13:32:33 +0300

    Added inspect command

---
+++ lib/App/PRF/Command/inspect.pm
@@ -0,0 +1,152 @@
+package App::PRF::Command::inspect;
+
+use strict;
+use warnings;
+
+use base 'App::PRF::Command';
+
+use Class::Load ();
+use PPI;
+use File::Find ();
+
+sub BUILD {
+    my $self = shift;
+
+    $self->{color} = 1 unless defined $self->{color};
+}
+
+sub run {
+    my $self = shift;
+    my ($root, $class) = @_;
+
+    die "Can't open '$root': $!\n" unless -d $root;
+
+    unshift @INC, $root;
+
+    my $path_to_class = $class . '.pm';
+    $path_to_class =~ s{::}{/}g;
+    $path_to_class = File::Spec->catfile($root, $path_to_class);
+
+    die "Can't open '$path_to_class': $!\n" unless -f $path_to_class;
+
+    my $ppi = PPI::Document->new($path_to_class, readonly => 1);
+
+    my $package = $ppi->find_first('PPI::Statement::Package');
+    next unless $package && $package->namespace;
+    $package = $package->namespace;
+
+    Class::Load::load_class($package);
+
+    my @isa = do {
+        no strict;
+        @{"$package\::ISA"};
+    };
+
+    print "Package:\n  $package\n";
+
+    if (@isa) {
+        print "ISA:\n";
+        print "  " . join ', ', @isa;
+        print "\n";
+    }
+
+    my $GREEN  = $self->{color} ? "\e[0;32m" : '';
+    my $YELLOW = $self->{color} ? "\e[0;33m" : '';
+    my $PURPLE = $self->{color} ? "\e[0;35m" : '';
+    my $RESET  = $self->{color} ? "\e[0m"    : '';
+
+    my @inherited_public_methods = $self->_get_inherited_public_methods($ppi, \@isa);
+    my @public_methods = $self->_get_public_methods($ppi);
+    if (@public_methods || @inherited_public_methods) {
+        print "Methods:\n";
+        foreach my $method (@inherited_public_methods) {
+            print "  $PURPLE$method->{name}$RESET ($method->{parent})\n";
+        }
+        foreach my $method (@public_methods) {
+            print "  $GREEN$method$RESET\n";
+        }
+    }
+
+    my @inherited_protected_methods = $self->_get_inherited_protected_methods($ppi, \@isa);
+    my @protected_methods = $self->_get_protected_methods($ppi);
+    if (@protected_methods || @inherited_protected_methods) {
+        print "Protected methods:\n";
+        foreach my $method (@inherited_protected_methods) {
+            print "  $PURPLE$method->{name}$RESET ($method->{parent})\n";
+        }
+        foreach my $method (@protected_methods) {
+            print "  $GREEN$method$RESET\n";
+        }
+    }
+}
+
+sub _get_inherited_public_methods {
+    my $self = shift;
+    my ($ppi, $isa) = @_;
+
+    my @result;
+
+    foreach my $parent (@$isa) {
+        my $path_to_class = $parent . '.pm';
+        $path_to_class =~ s{::}{/}g;
+        $path_to_class = $INC{$path_to_class};
+
+        my $ppi = PPI::Document->new($path_to_class, readonly => 1);
+
+        push @result,
+          map { {name => $_, parent => $parent} }
+          $self->_get_public_methods($ppi);
+    }
+
+    return @result;
+}
+
+sub _get_inherited_protected_methods {
+    my $self = shift;
+    my ($ppi, $isa) = @_;
+
+    my @result;
+
+    foreach my $parent (@$isa) {
+        my $path_to_class = $parent . '.pm';
+        $path_to_class =~ s{::}{/}g;
+        $path_to_class = $INC{$path_to_class};
+
+        my $ppi = PPI::Document->new($path_to_class, readonly => 1);
+
+        push @result,
+          map { {name => $_, parent => $parent} }
+          $self->_get_protected_methods($ppi);
+    }
+
+    return @result;
+}
+
+sub _get_public_methods {
+    my $self = shift;
+    my ($ppi) = @_;
+
+    return grep { !m/^_/ } $self->_get_package_methods($ppi);
+}
+
+sub _get_protected_methods {
+    my $self = shift;
+    my ($ppi) = @_;
+
+    return grep { m/^_/ } $self->_get_package_methods($ppi);
+}
+
+sub _get_package_methods {
+    my $self = shift;
+    my ($ppi) = @_;
+
+    my @result;
+    my $methods = 
+      $ppi
+      ->find(sub { $_[1]->isa('PPI::Statement::Sub') and $_[1]->name });
+    push @result, map { $_->name } @$methods;
+
+    return @result;
+}
+
+1;
---
+++ t/tests/inspectTest.pm
@@ -0,0 +1,102 @@
+package inspectTest;
+
+use strict;
+use warnings;
+
+use base 'TestBase';
+
+use Test::More;
+use Test::Fatal;
+
+use File::Path ();
+use File::Basename ();
+use File::Spec;
+
+use App::PRF::Command::inspect;
+
+sub inspect : Test {
+    my $self = shift;
+
+    my $command = $self->_build_command(root => $self->_create_root);
+
+    $self->_mkdir('Foo/Bar');
+    $self->_mkfile('Foo/Bar/Baz.pm', q/package Foo::Bar::Baz; sub new {} 1;/);
+
+    $self->_mkfile('Main.pm', q/package Main; use base 'Foo::Bar::Baz'; sub new {} 1;/);
+
+    my $output = '';
+
+    open my $stdout, ">&", STDOUT;
+    close STDOUT;
+    open STDOUT, '>', \$output or die "Failed to capture stdout: $!";
+
+    $command->run($self->{root}, 'Foo::Bar::Baz');
+
+    close STDOUT;
+    open STDOUT, ">&", $stdout;
+    close $stdout;
+
+    is($output, <<'EOF');
+Package:
+  Foo::Bar::Baz
+Methods:
+  new
+EOF
+}
+
+sub _build_command {
+    my $self = shift;
+
+    return App::PRF::Command::inspect->new(color => 0, @_);
+}
+
+sub _slurp {
+    my $self = shift;
+    my ($file) = @_;
+
+    local $/;
+    open my $fh, '<', File::Spec->catfile($self->{root}, $file);
+    return <$fh>;
+}
+
+sub _mkdir {
+    my $self = shift;
+    my ($dir) = @_;
+
+    File::Path::make_path(File::Spec->catfile($self->{root}, $dir));
+
+    return;
+}
+
+sub _mkfile {
+    my $self = shift;
+    my ($file, $content) = @_;
+
+    open my $fh, '>', File::Spec->catfile($self->{root}, $file);
+    print $fh $content;
+
+    return;
+}
+
+sub _create_root {
+    my $self = shift;
+
+    my $root =
+      File::Spec->catfile(File::Basename::dirname(__FILE__), '../inspect');
+    $self->{root} = $root;
+
+    if (-e $root) {
+        if (-d $root) {
+            File::Path::remove_tree($root);
+        }
+        else {
+            die 'Smth is wrong';
+        }
+    }
+
+    mkdir $root;
+
+    return $self->{root};
+}
+
+1;
